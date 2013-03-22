3a48318d7e0efef6b2f91f400389f2929a2670ac
Arnaud BRETON <arnaud@unishared.com>
2013-03-22 12:14:40 +0100

    Current step ID not used and causing problems with AngularJS routes

--- bootstrap-tour.js
+++ bootstrap-tour.js
@@ -295,10 +295,10 @@
         }
         nav = [];
         if (step.prev >= 0) {
-          nav.push("<a href='#" + step.prev + "' class='prev'>" + options.labels.prev + "</a>");
+          nav.push("<a href='#' class='prev'>" + options.labels.prev + "</a>");
         }
         if (step.next >= 0) {
-          nav.push("<a href='#" + step.next + "' class='next'>" + options.labels.next + "</a>");
+          nav.push("<a href='#' class='next'>" + options.labels.next + "</a>");
         }
         content += nav.join(" | ");
         content += "<a href='#' class='pull-right end'>" + options.labels.end + "</a>";
