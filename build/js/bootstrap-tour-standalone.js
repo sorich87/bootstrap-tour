4557237c9047df813ff01a8a4825dd0e861db929
mdouglasmorrison <mdouglasmorrison@gmail.com>
2014-10-08 10:55:07 -0400

    Add resize overlay method

--- build/js/bootstrap-tour-standalone.js
+++ build/js/bootstrap-tour-standalone.js
@@ -905,6 +905,10 @@
       }
       return this;
     };
+    
+    Tour.prototype.resizeOverlay = function() {
+      return this._showOverlayElement(this.getStep(this.getCurrentStep()).element, true);
+    };
 
     Tour.prototype._setState = function(key, value) {
       var e, keyName;
@@ -1245,10 +1249,10 @@
       }
     };
 
-    Tour.prototype._showOverlayElement = function(element) {
+    Tour.prototype._showOverlayElement = function(element, force) {
       var $background, $element, offset;
       $element = $(element);
-      if (!$element || $element.length === 0 || this.backdrop.overlayElementShown) {
+      if (!$element || $element.length === 0 || this.backdrop.overlayElementShown && !force) {
         return;
       }
       this.backdrop.overlayElementShown = true;
