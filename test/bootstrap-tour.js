da3c4499bceefae194fd3c13c4463b72ca4c97d2
Boris KOUMONDJI <brexis2009@yahoo.fr>
2015-05-15 22:03:34 +0100

    Add documentation for onRedirectError option

--- test/bootstrap-tour.js
+++ test/bootstrap-tour.js
@@ -59,7 +59,8 @@
         onNext: function(tour) {},
         onPrev: function(tour) {},
         onPause: function(tour, duration) {},
-        onResume: function(tour, duration) {}
+        onResume: function(tour, duration) {},
+        onRedirectError: function(tour) {}
       }, options);
       this._force = false;
       this._inited = false;
@@ -116,7 +117,8 @@
           onNext: this._options.onNext,
           onPrev: this._options.onPrev,
           onPause: this._options.onPause,
-          onResume: this._options.onResume
+          onResume: this._options.onResume,
+          onRedirectError: this._options.onRedirectError
         }, this._options.steps[i]);
       }
     };
@@ -202,6 +204,7 @@
     Tour.prototype.restart = function() {
       this._removeState('current_step');
       this._removeState('end');
+      this._removeState('redirect_to');
       return this.start();
     };
 
@@ -303,7 +306,7 @@
           }).call(_this);
           current_path = [document.location.pathname, document.location.hash].join('');
           if (_this._isRedirect(path, current_path)) {
-            _this._redirect(step, path);
+            _this._redirect(step, i, path);
             return;
           }
           if (_this._isOrphan(step)) {
@@ -457,12 +460,21 @@
       return (path != null) && path !== '' && (({}.toString.call(path) === '[object RegExp]' && !path.test(currentPath)) || ({}.toString.call(path) === '[object String]' && path.replace(/\?.*$/, '').replace(/\/?$/, '') !== currentPath.replace(/\/?$/, '')));
     };
 
-    Tour.prototype._redirect = function(step, path) {
+    Tour.prototype._redirect = function(step, i, path) {
       if ($.isFunction(step.redirect)) {
         return step.redirect.call(this, path);
       } else if (step.redirect === true) {
         this._debug("Redirect to " + path);
-        return document.location.href = path;
+        if (this._getState('redirect_to') === ("" + i)) {
+          this._debug("Error redirection loop to " + path);
+          this._removeState('redirect_to');
+          if (step.onRedirectError != null) {
+            return step.onRedirectError(this);
+          }
+        } else {
+          this._setState('redirect_to', "" + i);
+          return document.location.href = path;
+        }
       }
     };
 
